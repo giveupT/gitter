e44ddea7e2efad97d34d325c4b54958f1dc68c1d
Your Name <you@example.com>
2020-04-01 22:46:46 +0800

    third

--- homework_1.py
+++ homework_1.py
@@ -11,3 +11,16 @@ while t>=0:
     t+=1
 S=bs.select('ul#m-pl-container li div a.msk')
 A,m,B,C,D,E=[],'',[],[],{},{}
+for t in S:
+    A.append(t['href'])
+for y in A:
+    for u in y:
+        if u in ['1','2','3','4','5','6','7','8','9','0']:
+            m+=u
+    B.append(m)
+    m=''
+for i in S:
+    C.append(i['title'])
+ff=len(C)
+for e in range(0,ff):
+    E[B[e]]=C[e]    #id对名称的字典
